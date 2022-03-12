1e773c0bdf29003d8bec368131c8db2f0158afa5
LemonOrangeWasTaken <reallemonorange@gmail.com>
2022-03-12 11:09:03 -0800

    fixed some responsivness issues

--- assets/handlers/style.js
+++ assets/handlers/style.js
@@ -1,2 +1,5 @@
 // if user is on mobile
 let is_mobile = () => window.innerWidth < 900
+let isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+if(isSafari) document.querySelectorAll(".shadow-logo").forEach(e => e.classList.add("disable-hidden"))
